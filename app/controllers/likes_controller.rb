ca1a8758ab380d3fa991d7a7bdcbe3b0aea2d140
Jason Johnson <145226961+JJohnson606@users.noreply.github.com>
2024-03-16 04:14:46 +0000

    added likes ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -22,11 +22,13 @@ class LikesController < ApplicationController
   # POST /likes or /likes.json
   def create
     @like = Like.new(like_params)
+    @photo = Photo.find(like_params[:photo_id])
 
     respond_to do |format|
       if @like.save
         format.html { redirect_back fallback_location: root_url, notice: "Like was successfully created." }
         format.json { render :show, status: :created, location: @like }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @like.errors, status: :unprocessable_entity }
@@ -50,9 +52,11 @@ class LikesController < ApplicationController
   # DELETE /likes/1 or /likes/1.json
   def destroy
     @like.destroy
+    @photo = @like.photo
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Like was successfully destroyed." }
       format.json { head :no_content }
+      format.js
     end
   end
 
